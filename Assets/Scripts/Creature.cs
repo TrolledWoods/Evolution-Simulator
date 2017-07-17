d486fdc2bf33792f370fc7b1fbcbf8c751926224
John Hörnvall <trolledwoods@gmail.com>
2017-07-17 13:19:46 +0200

    Added a world controller

---
+++ Assets/Scripts/Creature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Creature
+    {
+
+        GameObject Controlling;
+
+        public Creature(GameObject controlling)
+        {
+            this.Controlling = controlling;
+
+            this.Controlling.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1));
+        }
+
+        public void Move()
+        {
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts;
+
+public class WorldController : MonoBehaviour {
+
+    public Object CreaturePrefab;
+    public int CreatureCount;
+
+    List<Creature> Creatures;
+
+	// Use this for initialization
+	void Start () {
+
+        Creatures = new List<Creature>();
+
+        for (int i = 0; i < CreatureCount; i++)
+        {
+            SpawnCreature(new Vector3(0, 0, 0));
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SpawnCreature(Vector3 Position){
+
+        GameObject T = (GameObject)GameObject.Instantiate(CreaturePrefab, Position, Quaternion.identity);
+        Creatures.Add(new Creature(T));
+
+    }
+}
